3b710c3761008b6e87751fd74cd914d5253deabd
TDR Manaswini <e0315869@u.nus.edu>
2019-08-29 17:47:31 +0800

    Level-2

--- src/main/java/ca/demo/terminal/Duke.java
+++ src/main/java/ca/demo/terminal/Duke.java
@@ -3,30 +3,41 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello from\n" + logo);
         System.out.println("------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------");
+        String[] storage= new String[100];
+        int i = 0;
+
 
         while (true) {
             Scanner in = new Scanner(System.in);
             String inputString = in.nextLine();
-            String t = "bye";
-            if (inputString.equals(t)) {
+            String bye = "bye"; String list = "list";
+            if (inputString.equals(bye)) {
                 System.out.println("------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("------------------------------");
                 break;
-            } else {
+            }
+            else if (inputString.equals(list)) {
                 System.out.println("------------------------------");
+                for (int j = 0; j < 100; j += 1) {
+                    if(storage[j] != null) {
+                        System.out.print((j + 1) + ". ");
+                        System.out.println(storage[j]);
+                    }
+                }
+                System.out.println("------------------------------");
+            }
+
+            else {
+                storage[i] = inputString;
+                i = i + 1;
+                System.out.println("------------------------------");
+                System.out.print("added: ");
                 System.out.println(inputString);
                 System.out.println("------------------------------");
             }
