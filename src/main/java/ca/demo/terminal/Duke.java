647b2f9caf1db2f9a74216f4e3cc9741f9ec66f2
TDR Manaswini <e0315869@u.nus.edu>
2019-08-30 00:50:48 +0800

    Level-3

--- src/main/java/ca/demo/terminal/Duke.java
+++ src/main/java/ca/demo/terminal/Duke.java
@@ -1,4 +1,6 @@
 package ca.demo.terminal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,34 +10,53 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------");
-        String[] storage= new String[100];
+        //String[] storage= new String[100];
+        ArrayList<Task> mytasks = new ArrayList<Task>();
         int i = 0;
-
 
         while (true) {
             Scanner in = new Scanner(System.in);
             String inputString = in.nextLine();
-            String bye = "bye"; String list = "list";
+            String bye = "bye"; String list = "list"; String check = inputString.split(" ")[0];
+            String done = "done";
+
             if (inputString.equals(bye)) {
                 System.out.println("------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("------------------------------");
                 break;
             }
+
             else if (inputString.equals(list)) {
                 System.out.println("------------------------------");
-                for (int j = 0; j < 100; j += 1) {
-                    if(storage[j] != null) {
-                        System.out.print((j + 1) + ". ");
-                        System.out.println(storage[j]);
-                    }
+                System.out.println("Here are the tasks in your list:");
+                for (int j = 0; j < mytasks.size(); j += 1) {
+                    System.out.print((j + 1) + ". [" );
+                    System.out.print(mytasks.get(j).getStatusIcon() + "] ");
+                    System.out.println(mytasks.get(j).description());
                 }
                 System.out.println("------------------------------");
             }
 
+            else if (check.equals(done)) {
+                char[] chars = inputString.toCharArray();
+                StringBuilder sb = new StringBuilder();
+                for(char c : chars) {
+                    if(Character.isDigit(c)) {
+                        sb.append(c);
+                    }
+                }
+
+                int result = Integer.parseInt(String.valueOf(sb));
+                mytasks.get(result - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.print("[" + mytasks.get(result - 1).getStatusIcon() + "] ");
+                System.out.println(mytasks.get(result - 1).description());
+            }
+
             else {
-                storage[i] = inputString;
-                i = i + 1;
+                Task t = new Task(inputString);
+                mytasks.add(t);
                 System.out.println("------------------------------");
                 System.out.print("added: ");
                 System.out.println(inputString);
---
+++ src/main/java/ca/demo/terminal/Task.java
@@ -0,0 +1,25 @@
+package ca.demo.terminal;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    //...
+
+    public String description() {
+        return this.description;
+    }
+}
